5394dcf445715db4b7f198f24f351cd4510b1879
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-11 20:05:28 +0200

    +

--- src/components/Cell/RenderFooter.js
+++ src/components/Cell/RenderFooter.js
@@ -9,9 +9,16 @@ import { Button, Icon, Layout, Row, Col } from 'antd'
 //@TODO it should be an updated we don't need to import three buttons, just because we pass a different layout to exporter
 
 
-import { GeneratePDFButton } from '../GroceryListToPDF'
-import { GeneratePDFButton2 } from '../GroceryListToPDF'
-import { GeneratePDFButton3 } from '../GroceryListToPDF'
+import {
+  GeneratePDFButton,
+  GeneratePDFButton2,
+  GeneratePDFButton3
+ } from '@groceristar/pdf-export';
+
+//
+// import { GeneratePDFButton } from '../GroceryListToPDF'
+// import { GeneratePDFButton2 } from '../GroceryListToPDF'
+// import { GeneratePDFButton3 } from '../GroceryListToPDF'
 
 
 
@@ -23,7 +30,9 @@ const { Footer } = Layout;
 function getLink( id ){
   return '/grocery/'+ id;
 }
+
 class RenderFooter extends PureComponent {
+
   // shouldComponentUpdate(nextProps, nextState) {
   // if (this.props.name !== nextProps.name || this.props.id !== nextProps.id) {
   //   console.log("Name and Id RenderFooter **********************");
@@ -32,8 +41,10 @@ class RenderFooter extends PureComponent {
   //     console.log("False RenderFooter****************************");
   //     return false;
   //   }
+
   render() {
-    return(
+
+    return (
       <Footer style={{ textAlign: 'center' }}>
 
 
@@ -45,14 +56,11 @@ class RenderFooter extends PureComponent {
               @TODO bad layout here - we need to overmake it as possible - right now it's just working*/}
 
               <GeneratePDFButton name={this.props.name} />
-
           </Col>
           <Col className="gutter-row" span={6}>
-              {/* our second button can be here */}
               <GeneratePDFButton2 name={this.props.name} />
           </Col>
           <Col className="gutter-row" span={6}>
-              {/* our third button can be here */}
               <GeneratePDFButton3 name={this.props.name} />
           </Col>
           <Col className="gutter-row" span={6}>
--- src/components/GroceryListToPDF/index.js
+++ src/components/GroceryListToPDF/index.js
@@ -6,11 +6,17 @@ import GeneratePDFButton from './GeneratePDFButton/GeneratePDFButton';
 import GeneratePDFButton2 from './GeneratePDFButton2/GeneratePDFButton2';
 import GeneratePDFButton3 from './GeneratePDFButton3/GeneratePDFButton3';
 
-import DocumentLayout    from './DownloadPDFLink/DocumentLayout';
-import DocumentLayout2    from './DownloadPDFLink2/DocumentLayout2';
-import DocumentLayout3    from './DownloadPDFLink3/DocumentLayout3';
+
+// import DocumentLayout    from './DownloadPDFLink/DocumentLayout';
+//
+// import DocumentLayout2    from './DownloadPDFLink2/DocumentLayout2';
+//
+// import DocumentLayout3    from './DownloadPDFLink3/DocumentLayout3';
+
 
 import RenderLists       from './RenderLists/RenderLists';
+
+
 import RenderLists2       from './RenderLists2/RenderLists2';
 import RenderLists3       from './RenderLists3/RenderLists3';
 
@@ -24,9 +30,10 @@ export {
   GeneratePDFButton,
   GeneratePDFButton2,
   GeneratePDFButton3,
-  DocumentLayout,
-  DocumentLayout2,
-  DocumentLayout3,
+  // DocumentLayout,
+  // DocumentLayout2,
+  // DocumentLayout3,
+
   RenderLists,
   RenderLists2,
   RenderLists3
